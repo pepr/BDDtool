d6785a403a831a433211f072fffbc89ff2afadd1
Petr Prikryl <prikryl@atlas.cz>
2014-01-05 14:37:40 +0100

    Založen docelement.py -- rozpoznávání typů řádků

---
+++ docelement.py
@@ -0,0 +1,109 @@
+#!python3
+# -*- coding: utf-8 -*-
+
+import re
+'''Capturing one line of a document and converting it to an object.'''
+
+
+class Element:
+    '''Rozpoznané Elementy dokumentu odpovídající řádkům zdrojového textu.'''
+
+    # Pro detekci řádku se stručným popisem požadavku. Na výstup se opisuje
+    # jako komentář.
+    rexFeature = re.compile('^\s*(Feature|Požadavek)\s*:', re.IGNORECASE)
+    
+    # Pro detekci prvního řádku s user story. Tento a následující řádky
+    # se na výstup se opisují jako komentář.
+    rexUserStory = re.compile('^\s*(User story)\s*:', re.IGNORECASE)
+    
+    # Pro detekci řádku s identifikací scénáře a pro extrakci textu.
+    # Následující regulární výrazy pak detekují další součásti popisu
+    # scénáře, které se transformují do podoby příslušných Catch sekcí.
+    rexScenario = re.compile('''^\s*(Scenario|Example|Scénář|Příklad)\s*:\s*
+                                    (?P<text>.+)$''',
+                             re.IGNORECASE | re.VERBOSE)
+    rexGiven = re.compile('''^\s*(Given|Dáno)\s*:\s*
+                                 (?P<text>.+)\s*$''',
+                             re.IGNORECASE | re.VERBOSE)
+    rexWhen = re.compile('''^\s*(When|Když)\s*:\s*
+                                (?P<text>.+)\s*$''',
+                             re.IGNORECASE | re.VERBOSE)
+    rexThen = re.compile('''^\s*(Then|Pak)\s*:\s*
+                                (?P<text>.+)\s*$''',
+                             re.IGNORECASE | re.VERBOSE)
+
+    def __init__(self, fname, lineno, line):
+        self.fname = fname      # původní zdrojový soubor
+        self.lineno = lineno    # číslo řádku ve zdrojovém souboru
+        self.line = line        # původní řádek
+
+        self.type = None        # typ elementu
+        self.attrib = None      # init -- atributy elementu (význam dle typu)
+
+        # Řádek obsahující jen whitespace považujeme za prázdný
+        # řádek ve významu oddělovače.
+        if self.line.isspace():
+            self.type = 'empty'
+            self.attrib = ''   # reprezentací bude prázdný řetězec
+            return
+
+        # Řádek s Feature.
+        m = self.rexFeature.search(line)
+        if m:
+            self.type = 'feature'
+            self.attrib = line.rstrip()
+            return
+
+        # Řádek s User Story.
+        m = self.rexUserStory.search(line)
+        if m:
+            self.type = 'userstory'
+            self.attrib = line.rstrip()
+            return
+
+        # Řádek s identifikací scénáře. Další řádky s Given, When, Then
+        # mají stejnou strukturu.
+        m = self.rexScenario.match(line)
+        if m:
+            self.type = 'scenario'
+            self.attrib = m.group('text').rstrip()
+            return
+
+        m = self.rexGiven.match(line)
+        if m:
+            self.type = 'given'
+            self.attrib = m.group('text').rstrip()
+            return
+
+        m = self.rexWhen.match(line)
+        if m:
+            self.type = 'when'
+            self.attrib = m.group('text').rstrip()
+            return
+
+        m = self.rexThen.match(line)
+        if m:
+            self.type = 'then'
+            self.attrib = m.group('text').rstrip()
+            return
+
+        # Prázdný řádek odpovídá situaci, kdy skončil soubor a další řádek
+        # nebylo možno načíst. Neměl by nastávat, ale pro jistotu.
+        if self.line == '':
+            # Z pohledu řešeného problému to tedy není prázdný řádek
+            # ve významu oddělovače.
+            self.type = 'EOF'
+            self.attrib = None
+            return
+
+        # Ostatní případy budeme považovat za řádek.
+        self.type = 'line'
+        self.attrib = line.rstrip()
+
+
+    def __repr__(self):
+        return repr((self.fname, self.lineno, self.type, self.attrib))
+
+
+    def __str__(self):
+        return self.line
