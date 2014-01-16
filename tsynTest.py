1de31f4d9ae157ba8a62788ea8354f7ade6292ac
Petr Prikryl <prikryl@atlas.cz>
2014-01-16 12:08:53 +0100

    BDDsyncatchTest.py renamed to tsynTest.py and modified

--- tsynTest.py
+++ tsynTest.py
@@ -4,11 +4,17 @@ import os
 import textwrap
 import unittest
 
-import lexcatch
+import tsyn
 
 class SyntaxCatchTests(unittest.TestCase):
     '''Testing syntax analysis for the Catch test sources.'''
 
+
+    def test_empty(self):
+        '''empty Catch test source'''
+        
+        source = ''
+        
 
 if __name__ == '__main__':
     unittest.main()
