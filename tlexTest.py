e95d110abd63cf8578c3c41f3bc6944ef250f329
Petr Prikryl <prikryl@atlas.cz>
2014-01-16 12:08:07 +0100

    lexanTest.py renamed to tlexTest.py and modified

--- tlexTest.py
+++ tlexTest.py
@@ -4,9 +4,9 @@ import os
 import textwrap
 import unittest
 
-import lexan
+import tlex
 
-class LexanTests(unittest.TestCase):
+class LexAnalyzerForCatchTests(unittest.TestCase):
     '''Testing lex analyzer for the Catch test sources.'''
 
     def setUp(self):
@@ -16,8 +16,10 @@ class LexanTests(unittest.TestCase):
     def test_empty_source(self):
         '''empty source for lexical analysis'''
         source = ''   # empty string as a source
-        lst = list(lexan.Container(source))
-        self.assertEqual(len(lst), 0)
+
+        lst = list(tlex.Container(source))
+        self.assertEqual(len(lst), 1)
+        self.assertEqual(lst[0], ('endofdata', '', '', None))
 
 
     def test_comment(self):
@@ -28,28 +30,31 @@ class LexanTests(unittest.TestCase):
         #
         # The shortest comment (empty) till the end of data.
         source = '//'
-        lst = list(lexan.Container(source))
-        self.assertEqual(len(lst), 1)   # single item
+        lst = list(tlex.Container(source))
+        print(lst)
+        self.assertEqual(len(lst), 2)
         item = lst[0]          # (symbol, lexem, pre, post)
-        self.assertEqual(item, ('comment', '', '//', None) )
+        self.assertEqual(lst, [('comment', '', '//', None),
+                               ('endofdata', '', '', None)
+                              ])
 
         # Empty comment till the end of line (newline included).
         source = '//\n'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]          # (symbol, lexem, pre, post)
         self.assertEqual(item, ('comment', '\n', '//', None) )
 
         # Non-empty comment with spaces in front of it.
         source = ' // a comment \n'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]          # (symbol, lexem, pre, post)
         self.assertEqual(item, ('comment', ' a comment \n', ' //', None) )
 
         # Two comments with spaces (two lines).
         source = ' // a comment 1 \n // a comment 2 \n'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 2)   # two items
         self.assertEqual(lst[0], ('comment', ' a comment 1 \n', ' //', None) )
         self.assertEqual(lst[1], ('comment', ' a comment 2 \n', ' //', None) )
@@ -59,7 +64,7 @@ class LexanTests(unittest.TestCase):
         #
         # The shortest unclosed comment till the end of data (error).
         source = '/*'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # one error item
         self.assertEqual(lst[0], ('error',
                                   "'*/' expected",
@@ -68,20 +73,20 @@ class LexanTests(unittest.TestCase):
 
         # The shortest comment (empty) till the end of data.
         source = '/**/'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # two items
         self.assertEqual(lst[0], ('comment', '', '/*', '*/') )
 
         # Empty comment till the end of line (newline).
         source = '/**/\n'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 2)
         self.assertEqual(lst[0], ('comment', '', '/*', '*/') )
         self.assertEqual(lst[1], ('emptyline', '\n', '', None) )
 
         # Empty comment till the end of line -- newline and space.
         source = '/**/\n '
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 3)
         self.assertEqual(lst[0], ('comment', '', '/*', '*/') )
         self.assertEqual(lst[1], ('emptyline', '\n', '', None) )
@@ -89,20 +94,20 @@ class LexanTests(unittest.TestCase):
 
         # Empty comment till the end of line -- space and newline.
         source = '/**/ \n'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 2)
         self.assertEqual(lst[0], ('comment', '', '/*', '*/') )
         self.assertEqual(lst[1], ('emptyline', ' \n', '', None) )
 
         # Non-empty comment with spaces.
         source = ' /* a comment */'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         self.assertEqual(lst[0], ('comment', ' a comment ', ' /*', '*/') )
 
         # A comment with spaces -- three lines.
         source = ' /* a comment 1 \n    a comment 2 \n */'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)
         self.assertEqual(lst[0], ('comment',
                          ' a comment 1 \n    a comment 2 \n ',
@@ -110,13 +115,13 @@ class LexanTests(unittest.TestCase):
 
         # A comment with extra stars.
         source = '/*** a comment ***/'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         self.assertEqual(lst[0], ('comment', '** a comment **', '/*', '*/') )
 
         # A comment with stars inside (separated from open/close sequences.
         source = '/* * * a comment * * */'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         self.assertEqual(lst[0], ('comment', ' * * a comment * * ', '/*', '*/') )
 
@@ -125,32 +130,32 @@ class LexanTests(unittest.TestCase):
         '''Catch identifiers considered keywords.'''
 
         # Keywords as exact strings with the exact case.
-        lst = list(lexan.Container('SCENARIO'))
+        lst = list(tlex.Container('SCENARIO'))
         self.assertEqual(len(lst), 1)   # single item
         self.assertEqual(lst[0], ('scenario', 'SCENARIO', '', None) )
 
-        lst = list(lexan.Container('GIVEN'))
+        lst = list(tlex.Container('GIVEN'))
         self.assertEqual(len(lst), 1)   # single item
         self.assertEqual(lst[0], ('given', 'GIVEN', '', None) )
 
-        lst = list(lexan.Container('WHEN'))
+        lst = list(tlex.Container('WHEN'))
         self.assertEqual(len(lst), 1)   # single item
         self.assertEqual(lst[0], ('when', 'WHEN', '', None) )
 
-        lst = list(lexan.Container('THEN'))
+        lst = list(tlex.Container('THEN'))
         self.assertEqual(len(lst), 1)   # single item
         self.assertEqual(lst[0], ('then', 'THEN', '', None) )
 
-        lst = list(lexan.Container('TEST_CASE'))
+        lst = list(tlex.Container('TEST_CASE'))
         self.assertEqual(len(lst), 1)   # single item
         self.assertEqual(lst[0], ('test_case', 'TEST_CASE', '', None) )
 
-        lst = list(lexan.Container('SECTION'))
+        lst = list(tlex.Container('SECTION'))
         self.assertEqual(len(lst), 1)   # single item
         self.assertEqual(lst[0], ('section', 'SECTION', '', None) )
 
         # Keyword with spaces around.
-        lst = list(lexan.Container(' SCENARIO '))
+        lst = list(tlex.Container(' SCENARIO '))
         self.assertEqual(len(lst), 2)   # two items
         self.assertEqual(lst[0], ('scenario', 'SCENARIO', ' ', None) )
         self.assertEqual(lst[1], ('skip', ' ', '', None) )
@@ -164,41 +169,41 @@ class LexanTests(unittest.TestCase):
 
         # Empty string literal.
         source = '""'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]          # (symbol, lexem, pre, post)
         self.assertEqual(item, ('stringlit', '', '"', '"') )
 
         # Simple string literal.
         source = '"simple"'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]
         self.assertEqual(item, ('stringlit', 'simple', '"', '"') )
 
         source = '"words and spaces"'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]
         self.assertEqual(item, ('stringlit', 'words and spaces', '"', '"') )
 
         # Escaped double quote.
         source = r'"\""'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]
         self.assertEqual(item, ('stringlit', r'\"', '"', '"') )
 
         # Escaped tab.
         source = r'"\t"'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]
         self.assertEqual(item, ('stringlit', r'\t', '"', '"') )
 
         # Escaped newline.
         source = r'"\n"'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]
         self.assertEqual(item, ('stringlit', r'\n', '"', '"') )
@@ -208,37 +213,37 @@ class LexanTests(unittest.TestCase):
         '''recognizing one-char terminal symbols'''
 
         source = '('
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]          # (symbol, lexem, pre, post)
         self.assertEqual(item, ('lpar', '(', '', None) )
 
         source = '\t('
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]
         self.assertEqual(item, ('lpar', '(', '\t', None) )
 
         source = ')'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]
         self.assertEqual(item, ('rpar', ')', '', None) )
 
         source = '{'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]
         self.assertEqual(item, ('lbrace', '{', '', None) )
 
         source = '}'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]
         self.assertEqual(item, ('rbrace', '}', '', None) )
 
         source = ','
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 1)   # single item
         item = lst[0]
         self.assertEqual(item, ('comma', ',', '', None) )
@@ -248,7 +253,7 @@ class LexanTests(unittest.TestCase):
         '''recognizing simple sections with empty body (lexical)'''
 
         source = 'TEST_CASE("identifier"){}'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 6)   # six items
         self.assertEqual(lst, [('test_case', 'TEST_CASE', '', None),
                                ('lpar', '(', '', None),
@@ -259,7 +264,7 @@ class LexanTests(unittest.TestCase):
                               ])
 
         source = 'SCENARIO("identifier"){}'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 6)   # six items
         self.assertEqual(lst, [('scenario', 'SCENARIO', '', None),
                                ('lpar', '(', '', None),
@@ -270,7 +275,7 @@ class LexanTests(unittest.TestCase):
                               ])
 
         source = 'SECTION("identifier"){}'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 6)   # six items
         self.assertEqual(lst, [('section', 'SECTION', '', None),
                                ('lpar', '(', '', None),
@@ -281,7 +286,7 @@ class LexanTests(unittest.TestCase):
                               ])
 
         source = 'GIVEN("identifier"){}'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 6)   # six items
         self.assertEqual(lst, [('given', 'GIVEN', '', None),
                                ('lpar', '(', '', None),
@@ -292,7 +297,7 @@ class LexanTests(unittest.TestCase):
                               ])
 
         source = 'WHEN("identifier"){}'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 6)   # six items
         self.assertEqual(lst, [('when', 'WHEN', '', None),
                                ('lpar', '(', '', None),
@@ -303,7 +308,7 @@ class LexanTests(unittest.TestCase):
                               ])
 
         source = 'THEN("identifier"){}'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 6)   # six items
         self.assertEqual(lst, [('then', 'THEN', '', None),
                                ('lpar', '(', '', None),
@@ -314,7 +319,7 @@ class LexanTests(unittest.TestCase):
                               ])
 
         source = 'AND_THEN("identifier"){}'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 6)   # six items
         self.assertEqual(lst, [('and_then', 'AND_THEN', '', None),
                                ('lpar', '(', '', None),
@@ -325,7 +330,7 @@ class LexanTests(unittest.TestCase):
                               ])
 
         source = 'AND_WHEN("identifier"){}'
-        lst = list(lexan.Container(source))
+        lst = list(tlex.Container(source))
         self.assertEqual(len(lst), 6)   # six items
         self.assertEqual(lst, [('and_when', 'AND_WHEN', '', None),
                                ('lpar', '(', '', None),
@@ -368,7 +373,7 @@ class LexanTests(unittest.TestCase):
 #        '''story or feature recognition inside the comment'''
 #
 #        source = '// Story: story identifier'
-#        lst = list(lexan.Container(source))
+#        lst = list(tlex.Container(source))
 #        self.assertEqual(len(lst), 1)   # single item
 #        item = lst[0]
 #        self.assertEqual(item, ('story', 'story identifier', '// Story: ', None))
